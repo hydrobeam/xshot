dfd5ccc1e5e3e7627e2b4fa282ae7e7e0433dfcb
Laith Bahodi <laithbahodi@gmail.com>
2023-07-16 00:57:51 -0400

    doc: improve the look of cli --help

--- src/cli.rs
+++ src/cli.rs
@@ -2,38 +2,32 @@ use clap::{Parser, ValueEnum};
 use image::ImageOutputFormat;
 
 #[derive(Parser, Debug)]
-#[command(name = "x-sc")]
+#[command(name = "xsshot")]
 #[command(author = "Laith Bahodi <laithbahodi@gmail.com>")]
-#[command(about = "Basic screenshot tool for X")]
+#[command(about = "The XS screenshot tool for X")]
 #[command(author, version, about, long_about=None)]
 pub struct Cli {
     /// The window name to target.
     ///
-    /// Defaults to the full screen.
-    ///
     /// Queries the _NET_WM_NAME property (i.e. the full display name of the window):
+    ///
     /// https://specifications.freedesktop.org/wm-spec/1.3/ar01s05.html
     ///
     /// Matching is fuzzy, a screenshot will be taken of the first match.
-    ///
-    /// Queries the
     #[arg(short, long, conflicts_with = "class")]
     pub name: Option<String>,
 
     /// The window class to target. Incomptabile with `name`.
     ///
-    /// Defaults to the full screen.
-    ///
     /// Queries the WM_CLASS property (i.e. the overall class of application: "Emacs", "Firefox", ...):
+    ///
     /// https://tronche.com/gui/x/icccm/sec-4.html#WM_CLASS
     ///
     /// Matching is fuzzy, a screenshot will be taken of the first match.
     #[arg(short, long, conflicts_with = "name")]
     pub class: Option<String>,
 
-    /// Where the topleft corner of the screenshot is located.
-    ///
-    /// Defaults to (0, 0).
+    /// The topleft corner of the screenshot
     #[arg(short, long, num_args(2), default_values=["0", "0"])]
     pub position: Vec<i16>,
 
@@ -43,6 +37,7 @@ pub struct Cli {
     #[arg(short, long, num_args(2))]
     pub size: Option<Vec<u16>>,
 
+    /// The image format for the screenshot
     #[arg(short, long, value_enum, default_value = "png")]
     pub format: OutputFormat,
 }
